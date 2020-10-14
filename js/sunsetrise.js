500123bd372e2da9adbc3194ffb794a5e1caeeb4
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-14 20:30:46 +0200

    [WDFT-BCN-0920] 1st prj v2.3 (initial version) cities and diffs working and map and list of cities ongoing

--- js/sunsetrise.js
+++ js/sunsetrise.js
@@ -276,9 +276,51 @@ handleCity1Input = (event) => {
   //console.log("City1Index: ", city1Index);
 
   getLatLon(cityLatLong[city1Index].cityLat, cityLatLong[city1Index].cityLon, 1);
+  //city1map();
   //console.log("Back from API: ", apiListCity1);
   //renderInfoCity1(apiListCity1);
 };
+
+// // STARTS HERE map - code got from their API.
+
+// function moveMapToBerlin(map){
+//   map.setCenter({lat: 52.5159, lng: 13.3777});
+//   map.setZoom(14);
+// }
+
+// //Step 1: initialize communication with the platform
+// // In your own code, replace variable window.apikey with your own apikey
+// function city1map () {
+//   var platform = new H.service.Platform({
+//     apikey: '8h5lXXll3zO_hFQT2b_jH8x9NOGoi5OjhgQ3w8JBVYA'
+//   });
+//   var defaultLayers = platform.createDefaultLayers();
+
+//   //Step 2: initialize a map - this map is centered over Europe
+//   var map = new H.Map(document.getElementById('map'),
+//     defaultLayers.vector.normal.map,{
+//     center: {lat:50, lng:5},
+//     zoom: 4,
+//     pixelRatio: window.devicePixelRatio || 1
+//   });
+//   // add a resize listener to make sure that the map occupies the whole container
+//   window.addEventListener('resize', () => map.getViewPort().resize());
+
+//   //Step 3: make the map interactive
+//   // MapEvents enables the event system
+//   // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+//   var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+//   // Create the default UI components
+//   var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+//   // Now use the map as required...
+//   window.onload = function () {
+//     moveMapToBerlin(map);
+//   };
+// }
+
+// // ENDS HERE map - code got from their API.
 
 handleCity2Input = (event) => {
   const city2Input = event.target.value;
@@ -353,9 +395,25 @@ function secondsToString(seconds) {
   return hour + ':' + minute + ':' + second;
 }
 
+function loadCities () {
+  var querySelected;
+  var node;
+  var textnode;
+  for (let i = 0; i < cityLatLong.length; i++) {
+    node = document.createElement("OPTION");
+    console.log (node);
+    node.setAttribute("value", cityLatLong[i].cityname);
+    console.log (node);
+    textnode = document.createTextNode(cityLatLong[i].cityname);
+    console.log(textnode);
+    node.appendChild(textnode);
+    console.log(node);
+    //document.querySelector('.city').appendChild(node);    
+  }
+}
 
 // window.addEventListener("load", signup.addCityListeners());
-
+loadCities();
 window.addEventListener("load", addCityListeners());
 
 
