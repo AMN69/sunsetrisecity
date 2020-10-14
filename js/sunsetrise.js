2bf5a07bca1d5965760f16988c35f6c9a6ffc08b
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-14 13:22:57 +0200

    [WDFT-BCN-0920] 1st prj v2.1 (initial version)

--- js/sunsetrise.js
+++ js/sunsetrise.js
@@ -141,10 +141,15 @@ const cityLatLong = [{
 
 
 //const api = 'https://api.sunrise-sunset.org/json?lat=42.38&lng=3.15';
-async function getLatLon(lat, lon) {
+async function getLatLon(lat, lon, city) {
   const api = "https://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon;
   const apiResponse = await fetch(api);
   const apiList = await apiResponse.json();
+  if (city === 1) {
+    await renderInfoCity1(apiList);  
+  } else {
+    await renderInfoCity2(apiList);
+  }
   return apiList;
 }
 
@@ -163,59 +168,68 @@ renderInfoCity1 = (apiListCity1) => {
   let dayLength1 = "Day length: " + apiListCity1.results.day_length;
   let civilTwilightBegin1 = "Twilight begins: " + apiListCity1.results.civil_twilight_begin;
   let civilTwilightEnd1 = "Twilight ends: " + apiListCity1.results.civil_twilight_end;
-
-  document.getElementsByClassName('ctime1').innerHTML(sunRise1);
-  document.getElementsByClassName('sstime1').innerHTML(sunSet1);
-  document.getElementsByClassName('sntime1').innerHTML(solarNoon1);
-  document.getElementsByClassName('dltime1').innerHTML(dayLength1);
-  document.getElementsByClassName('tbtime1').innerHTML(civilTwilightBegin1);
-  document.getElementsByClassName('tetime1').innerHTML(civilTwilightEnd1);
+  //console.log("sunRise1", sunRise1);
+  
+  document.querySelector('.srtime1').innerText = sunRise1;
+  document.querySelector('.sstime1').innerText = sunSet1;
+  document.querySelector('.sntime1').innerText = solarNoon1;
+  document.querySelector('.dltime1').innerText = dayLength1;
+  document.querySelector('.tbtime1').innerText = civilTwilightBegin1;
+  document.querySelector('.tetime1').innerText = civilTwilightEnd1;
 }
 
 renderInfoCity2 = (apiListCity2) => {
-  let sunRise2 = apiListCity1.results.sunrise;
-  let sunSet2 = apiListCity1.results.sunset;
-  let solarNoon2 = apiListCity1.results.solar_noon;
-  let dayLength2 = apiListCity1.results.day_length;
-  let civilTwilightBegin2 = apiListCity1.results.civil_twilight_begin;
-  let civilTwilightEnd2 = apiListCity1.results.civil_twilight_end;
+  let sunRise2 = "Sunrise: " + apiListCity2.results.sunrise;
+  let sunSet2 = "Sunset: " + apiListCity2.results.sunset;
+  let solarNoon2 = "Solar noon: " + apiListCity2.results.solar_noon;
+  let dayLength2 = "Day length: " + apiListCity2.results.day_length;
+  let civilTwilightBegin2 = "Twilight begins: " + apiListCity2.results.civil_twilight_begin;
+  let civilTwilightEnd2 = "Twilight ends: " + apiListCity2.results.civil_twilight_end;
 
-  document.getElementsByClassName('ctime2').innerHTML(sunRise2);
-  document.getElementsByClassName('sstime2').innerHTML(sunSet2);
-  document.getElementsByClassName('sntime2').innerHTML(solarNoon2);
-  document.getElementsByClassName('dltime2').innerHTML(dayLength2);
-  document.getElementsByClassName('tbtime2').innerHTML(civilTwilightBegin2);
-  document.getElementsByClassName('tetime2').innerHTML(civilTwilightEnd2);
+  document.querySelector('.srtime2').innerText = sunRise2;
+  document.querySelector('.sstime2').innerText = sunSet2;
+  document.querySelector('.sntime2').innerText = solarNoon2;
+  document.querySelector('.dltime2').innerText = dayLength2;
+  document.querySelector('.tbtime2').innerText = civilTwilightBegin2;
+  document.querySelector('.tetime2').innerText = civilTwilightEnd2;
 }
 
 handleCity1Input = (event) => {
   const city1Input = event.target.value;
+  //console.log('City1Input: ', city1Input);
   let city1Index;
   let i = 0;
   while (i < cityLatLong.length) {
-    if (city1Input == cityLatLong[i]) {
+    //console.log("Var i: ", i);
+    if (city1Input === cityLatLong[i].cityname) {
       city1Index = i;
       i = cityLatLong.length;
+      // console.log("Var i within if: ", i);
+      // console.log("Var city1Index: ", city1Index);
     }
     i++;
   }
-  let apiListCity1 = getLatLon(cityLatLong[city1Index].cityLat, cityLatLong[city1Index].cityLon);
-  
-  renderInfoCity1(apiListCity1);
+  //console.log("City1Index: ", city1Index);
+
+  getLatLon(cityLatLong[city1Index].cityLat, cityLatLong[city1Index].cityLon, 1);
+  //console.log("Back from API: ", apiListCity1);
+  //renderInfoCity1(apiListCity1);
 }
 
 handleCity2Input = (event) => {
   const city2Input = event.target.value;
+  let city2Index;
   let i = 0;
   while (i < cityLatLong.length) {
-    if (city2Input == cityLatLong[i]) {
+    if (city2Input === cityLatLong[i].cityname) {
+      city2Index = i;
       i = cityLatLong.length;
     }
     i++;
   }
-  let apiListCity2 = getLatLon(cityLatLong[i].cityLat, cityLatLong[i].cityLon);
+  getLatLon(cityLatLong[city2Index].cityLat, cityLatLong[city2Index].cityLon, 2);
 
-  renderInfoCity1(apiListCity1);
+  // renderInfoCity1(apiListCity1);
 }
 
 // window.addEventListener("load", signup.addCityListeners());
