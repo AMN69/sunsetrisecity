e7e084788e6468627195d324cbb18e01e184e3b8
Andreu Martínez <martinez.andreu@gmail.com>
2020-10-15 13:50:47 +0200

    [WDFT-BCN-0920] 1st prj v2.6 (initial version) cities and diffs working and map and list of cities ongoing

--- js/sunsetrise.js
+++ js/sunsetrise.js
@@ -178,7 +178,7 @@ async function getLatLon(lat, lon, city) {
     diffDayLength = await calculateDifferences(dayLength1Kept, dayLength2Kept);
     diffTwilightBeg = await calculateDifferences(civilTwilightBegin1Kept, civilTwilightBegin2Kept);
     diffTwilightEnd = await calculateDifferences(civilTwilightEnd1Kept, civilTwilightEnd2Kept);
-    await renderDiff();  
+    await renderDiff();
   }
   
   return apiList;
@@ -260,10 +260,12 @@ renderDiff = () => {
 
 function renderCity1map(city, country) {
   document.querySelector('.map-frame1').setAttribute("src", "https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=8h5lXXll3zO_hFQT2b_jH8x9NOGoi5OjhgQ3w8JBVYA&co=" + country + "&ci=" + city);
+  document.querySelector('.wiki-city1').setAttribute("href", "https://es.wikipedia.org/wiki/" + city);
 }
 
 function renderCity2map(city, country) {
   document.querySelector('.map-frame2').setAttribute("src", "https://image.maps.ls.hereapi.com/mia/1.6/mapview?apiKey=8h5lXXll3zO_hFQT2b_jH8x9NOGoi5OjhgQ3w8JBVYA&co=" + country + "&ci=" + city);
+  document.querySelector('.wiki-city2').setAttribute("href", "https://es.wikipedia.org/wiki/" + city);
 }
 
 handleCity1Input = (event) => {
@@ -288,47 +290,6 @@ handleCity1Input = (event) => {
   //console.log("Back from API: ", apiListCity1);
   //renderInfoCity1(apiListCity1);
 };
-
-// // STARTS HERE map - code got from their API.
-
-// function moveMapToBerlin(map){
-//   map.setCenter({lat: 52.5159, lng: 13.3777});
-//   map.setZoom(14);
-// }
-
-// //Step 1: initialize communication with the platform
-// // In your own code, replace variable window.apikey with your own apikey
-// function city1map () {
-//   var platform = new H.service.Platform({
-//     apikey: '8h5lXXll3zO_hFQT2b_jH8x9NOGoi5OjhgQ3w8JBVYA'
-//   });
-//   var defaultLayers = platform.createDefaultLayers();
-
-//   //Step 2: initialize a map - this map is centered over Europe
-//   var map = new H.Map(document.getElementById('map'),
-//     defaultLayers.vector.normal.map,{
-//     center: {lat:50, lng:5},
-//     zoom: 4,
-//     pixelRatio: window.devicePixelRatio || 1
-//   });
-//   // add a resize listener to make sure that the map occupies the whole container
-//   window.addEventListener('resize', () => map.getViewPort().resize());
-
-//   //Step 3: make the map interactive
-//   // MapEvents enables the event system
-//   // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
-//   var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-
-//   // Create the default UI components
-//   var ui = H.ui.UI.createDefault(map, defaultLayers);
-
-//   // Now use the map as required...
-//   window.onload = function () {
-//     moveMapToBerlin(map);
-//   };
-// }
-
-// // ENDS HERE map - code got from their API.
 
 handleCity2Input = (event) => {
   const city2Input = event.target.value;
@@ -404,6 +365,8 @@ function secondsToString(seconds) {
   return hour + ':' + minute + ':' + second;
 }
 
+// We load the cities within the drop-down buttons (two cities)
+
 function loadCities () {
   var querySelected;
   var node;
@@ -411,11 +374,19 @@ function loadCities () {
   for (let i = 0; i < cityLatLong.length; i++) {
     querySelected = document.querySelector('.city1');
     node = document.createElement('option');
-    console.log (node);
+    // console.log (node);
     node.setAttribute("value", cityLatLong[i].cityname);
-    console.log("city: ", cityLatLong[i].cityname)
+    // console.log("city: ", cityLatLong[i].cityname)
     node.innerText = cityLatLong[i].cityname;
-    console.log (node);
+    // console.log (node);
+    querySelected.appendChild(node);
+    querySelected = document.querySelector('.city2');
+    node = document.createElement('option');
+    // console.log (node);
+    node.setAttribute("value", cityLatLong[i].cityname);
+    // console.log("city: ", cityLatLong[i].cityname)
+    node.innerText = cityLatLong[i].cityname;
+    // console.log (node);
     querySelected.appendChild(node);
   }
 }
